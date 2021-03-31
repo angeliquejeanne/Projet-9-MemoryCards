f31c8ae7dbc895dc1c0ea910c59572dba4f3825a
anggiie <anggiie02>
2021-03-31 23:37:01 +0200

    add modifs

--- script.js
+++ script.js
@@ -9,44 +9,31 @@ cards.forEach(theCard => {
 })
 
 function revertCard(){
-
     if(locking) return;
-
     this.childNodes[1].classList.toggle('active');
-
     if(!returnCard){
-
         returnCard = true;
         firstCard = this;
         return;
     }
-
     returnCard = false;
     secondCard = this;
-
     // console.log(firstCard, secondCard);
-
     correspondence();
 }
 
 function correspondence(){
-
     if(firstCard.getAttribute('data-attr') === secondCard.getAttribute('data-attr')) {
-
         firstCard.removeEventListener('click', revertCard);
         secondCard.removeEventListener('click', revertCard);
-
     } else {
         locking = true;
         setTimeout(() => {
-
             firstCard.childNodes[1].classList.remove('active');
             secondCard.childNodes[1].classList.remove('active');
-
             locking = false;
         }, 1500)
     }
-
 }
 
 function random(){
